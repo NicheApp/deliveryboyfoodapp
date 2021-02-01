8f6f7f5a9f7627bb50a59e79a6967d6e2182b621
NicheApp <nishkarshmaitry0864@gmail.com>
2021-02-02 02:49:15 +0530

    push notification complete

--- app/src/main/java/com/mj/deliveryboyapp/MainActivity.java
+++ app/src/main/java/com/mj/deliveryboyapp/MainActivity.java
@@ -1,14 +1,110 @@
 package com.mj.deliveryboyapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RequestQueue mRequestQue;
+    private String URL = "https://fcm.googleapis.com/fcm/send";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (getIntent().hasExtra("category")){
+            Intent intent = new Intent(MainActivity.this,ReceiveNotificationActivity.class);
+            intent.putExtra("category",getIntent().getStringExtra("category"));
+            intent.putExtra("brandId",getIntent().getStringExtra("brandId"));
+            startActivity(intent);
+        }
+        Button button = findViewById(R.id.btn);
+        mRequestQue = Volley.newRequestQueue(this);
+        FirebaseMessaging.getInstance().subscribeToTopic("news");
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    sendNotification();
+
+
+            }
+        });
+
+    }
+
+
+
+    private void sendNotification() {
+
+        JSONObject json = new JSONObject();
+        try {
+            json.put("to","/topics/"+"news");
+            JSONObject notificationObj = new JSONObject();
+            notificationObj.put("title","");
+            notificationObj.put("body","any body");
+
+            JSONObject extraData = new JSONObject();
+            extraData.put("brandId","puma");
+            extraData.put("category","Shoes");
+
+            json.put("notification",notificationObj);
+           json.put("data",extraData);
+
+
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, URL,
+                    json,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+
+                            Log.d("MUR-------------------", "onResponse: "+response);
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("MUR", "onError:------ "+error.networkResponse);
+                }
+            }
+            ){
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String,String> header = new HashMap<>();
+                    header.put("content-type","application/json");
+                    header.put("authorization","key=AAAAT1XhxNM:APA91bE2ybeRH_7-qoXs-YTBeTuZiiuvBMA7EcN7PvFgLnEYUOAPlwD5aWZmGgDwb1QWuncdWki5KxuN1Qao3v_uTFB5ygDWBCksXbxM1LtN4fWVfaJSeauawbsGl8rNCfW-U0N7f8zH");
+                    return header;
+                }
+            };
+            mRequestQue.add(request);
+        }
+        catch (JSONException e)
+
+        {
+            Toast.makeText(this,"89898989"+e.toString(),Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
     }
 }
