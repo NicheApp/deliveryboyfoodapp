59e46d0fb393e9543226b62932e10e9d051981bb
NicheApp <nishkarshmaitry0864@gmail.com>
2021-02-06 23:38:12 +0530

    new version

--- app/src/main/java/com/mj/deliveryboyapp/MainActivity.java
+++ app/src/main/java/com/mj/deliveryboyapp/MainActivity.java
@@ -2,130 +2,49 @@ package com.mj.deliveryboyapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.transition.Transition;
-import android.transition.TransitionInflater;
-import android.util.Log;
-import android.view.View;
-import android.view.Window;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.messaging.FirebaseMessaging;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    private RequestQueue mRequestQue;
-    private String URL = "https://fcm.googleapis.com/fcm/send";
-    public  static List<orderdetailsmodelclass> orders;
-    TextView textView;
+    BottomNavigationView bottomNavigationView;
+    FrameLayout frameLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
         setContentView(R.layout.activity_main);
-        Transition explodeAnimation = TransitionInflater.from(this).inflateTransition(R.transition.slide);
-        explodeAnimation.setDuration(1000);
-        getWindow().setEnterTransition(explodeAnimation);
-      textView=findViewById(R.id.txt);
-
-        orders=new ArrayList<>();
-        if (getIntent().hasExtra("category")){
-            Intent intent = new Intent(MainActivity.this,ReceiveNotificationActivity.class);
-            intent.putExtra("category",getIntent().getStringExtra("category"));
-            intent.putExtra("brandId",getIntent().getStringExtra("brandId"));
-            startActivity(intent);
-        }
-        Button button = findViewById(R.id.btn);
-        mRequestQue = Volley.newRequestQueue(this);
-        SharedPreferences prefs=this.getSharedPreferences("MyPref",MODE_PRIVATE);
-        textView.setText(prefs.getString("available","0"));
-        if(prefs.getString("available","no").equals("no"))
-        {FirebaseMessaging.getInstance().subscribeToTopic("no");}
-      else  if(prefs.getString("available","no").equals("yes"))
-        FirebaseMessaging.getInstance().subscribeToTopic("yes");
-
-        button.setOnClickListener(new View.OnClickListener() {
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        frameLayout = findViewById(R.id.flFragment);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                FirebaseMessaging.getInstance().subscribeToTopic("yes");
-                  //  sendNotification();
-
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        setFragement(new Homefragment());
+                        return true;
+                    case R.id.history:
+                        setFragement(new Historyfragment());
+                        return true;
+                    case R.id.account:
+                        setFragement(new Accountfragment());
+                        return true;
+                    default:
+                        return false;
+                }
 
             }
         });
-
     }
 
-
-
-    private void sendNotification() {
-
-        JSONObject json = new JSONObject();
-        try {
-            json.put("to","/topics/"+"news");
-            JSONObject notificationObj = new JSONObject();
-            notificationObj.put("title","");
-            notificationObj.put("body","any body");
-
-            JSONObject extraData = new JSONObject();
-            extraData.put("brandId","puma");
-            extraData.put("category","Shoes");
-
-            json.put("notification",notificationObj);
-            json.put("data",extraData);
-
-
-            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, URL,
-                    json,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-
-                            Log.d("MUR-------------------", "onResponse: "+response);
-                        }
-                    }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Log.d("MUR", "onError:------ "+error.networkResponse);
-                }
-            }
-            ){
-                @Override
-                public Map<String, String> getHeaders() throws AuthFailureError {
-                    Map<String,String> header = new HashMap<>();
-                    header.put("content-type","application/json");
-                    header.put("authorization","key=AAAAT1XhxNM:APA91bE2ybeRH_7-qoXs-YTBeTuZiiuvBMA7EcN7PvFgLnEYUOAPlwD5aWZmGgDwb1QWuncdWki5KxuN1Qao3v_uTFB5ygDWBCksXbxM1LtN4fWVfaJSeauawbsGl8rNCfW-U0N7f8zH");
-                    return header;
-                }
-            };
-            mRequestQue.add(request);
-        }
-        catch (JSONException e)
-
-        {
-            Toast.makeText(this,"89898989"+e.toString(),Toast.LENGTH_SHORT).show();
-            e.printStackTrace();
-        }
+    private void setFragement(Fragment fragement) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.flFragment,fragement);
+        fragmentTransaction.commit();
     }
 }
